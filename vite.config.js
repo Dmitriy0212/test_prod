0eef6f4dd4afa3179be3069646106e773278ac8b
Dmitriy0212 <demarr68@gmail.com>
2026-04-06 02:12:36 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,38 +3,44 @@ import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
+import { fileURLToPath, URL } from 'node:url';
 
-export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '/' : '/cheerful_monk/',
-  root: 'src',
+export default defineConfig(({ command }) => {
+  const isServe = command === 'serve';
+  const basePath = isServe ? '/' : '/cheerful_monk/';
 
-  resolve: {
-    alias: {
-      '@': '/src',
-      '@img': '/src/img',
-      '@css': '/src/css',
-      '@js': '/src/js',
-    },
-  },
+  return {
+    base: basePath,
+    root: 'src',
 
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-
-    rollupOptions: {
-      input: glob.sync('./src/**/*.html'),
-
-      output: {
-        entryFileNames: 'assets/js/[name].js',
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash][extname]',
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '@img': fileURLToPath(new URL('./src/img', import.meta.url)),
+        '@css': fileURLToPath(new URL('./src/css', import.meta.url)),
+        '@js': fileURLToPath(new URL('./src/js', import.meta.url)),
       },
     },
-  },
 
-  plugins: [
-    injectHTML(),
-    FullReload(['./src/**/*.html']),
-    SortCss({ sort: 'mobile-first' }),
-  ],
-}));
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+
+      rollupOptions: {
+        input: glob.sync('**/*.html', { cwd: 'src', absolute: true }),
+
+        output: {
+          entryFileNames: 'assets/js/[name].js',
+          chunkFileNames: 'assets/js/[name]-[hash].js',
+          assetFileNames: 'assets/[name]-[hash][extname]',
+        },
+      },
+    },
+
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/*.html']),
+      SortCss({ sort: 'mobile-first' }),
+    ],
+  };
+});
